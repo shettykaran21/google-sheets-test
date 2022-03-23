5caf9d81e32a62cb6d56b29d600c68e33bc887f2
Karan Shetty <shettykaran2110@gmail.com>
2022-03-24 02:43:25 +0530

    Fill formula data and serial nos.

--- main.py
+++ main.py
@@ -15,70 +15,130 @@ creds = service_account.Credentials.from_service_account_file(
 SPREADSHEET_ID = '1g4Qn9gbpx810jHTvgEvGkAN7OOURNgzU04c-ydqPwUo'
 
 
+def fill_column_data(sheet, from_range, to_range):
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=from_range).execute()
+    values = result.get('values', [])
+    sheet.values().update(spreadsheetId=SPREADSHEET_ID, range=to_range,
+                          valueInputOption="USER_ENTERED", body={"values": values}).execute()
+
+    return len(values)
+
+
+def copy_paste_cells(sheet, source, destination):
+    request_body = {
+        "requests": [
+            {
+                "copyPaste": {
+                    "source": source,
+                    "destination": destination,
+                    "pasteType": "PASTE_NORMAL",
+                    "pasteOrientation": "NORMAL"
+                }
+            }
+        ]
+    }
+    sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID,
+                      body=request_body).execute()
+
+
+def add_borders(sheet, rows):
+    request_body = {"requests": [
+        {
+            "updateBorders": {
+                    "range": {
+                        "sheetId": 812605565,
+                        "startRowIndex": 0,
+                        "endRowIndex": rows,
+                        "startColumnIndex": 0,
+                        "endColumnIndex": 18
+                    },
+                    "top": {"style": "SOLID", "width": 1, "color": {"red": 0, "green": 0, "blue": 0}},
+                    "bottom": {"style": "SOLID", "width": 1, "color": {"red": 0, "green": 0, "blue": 0}},
+                    "right": {"style": "SOLID", "width": 1, "color": {"red": 0, "green": 0, "blue": 0}},
+                    "left": {"style": "SOLID", "width": 1, "color": {"red": 0, "green": 0, "blue": 0}},
+                    "innerHorizontal": {"style": "SOLID", "width": 1, "color": {"red": 0, "green": 0, "blue": 0}},
+                    }
+        }
+    ]}
+    sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID,
+                      body=request_body).execute()
+
+
+def fill_serial_nos(sheet, rows):
+    request_body = {
+        "requests": [
+            {
+                "repeatCell": {
+                    "range": {
+                        "sheetId": 812605565,
+                        "startRowIndex": 1,
+                        "endRowIndex": rows,
+                        "startColumnIndex": 0,
+                        "endColumnIndex": 1
+                    },
+                    "cell": {
+                        "userEnteredValue": {
+                            "formulaValue": "=ROW()-1"
+                        },
+                    },
+                    "fields": "userEnteredValue"
+                }
+            }
+        ]}
+    sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID,
+                      body=request_body).execute()
+
+
 def main():
     try:
         service = build('sheets', 'v4', credentials=creds)
 
         sheet = service.spreadsheets()
 
-        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                    range="Stocks_near_52_week_high!C3:C").execute()
-        symbols = result.get('values', [])
-        sheet.values().update(spreadsheetId=SPREADSHEET_ID, range="21st Mar!C2",
-                              valueInputOption="USER_ENTERED", body={"values": symbols}).execute()
+        no_of_stocks = fill_column_data(
+            sheet, from_range="Stocks_near_52_week_high!C3:C", to_range="21st Mar!C2")
 
-        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                    range="Stocks_near_52_week_high!F3:F").execute()
-        price_on_appearance = result.get('values', [])
-        sheet.values().update(spreadsheetId=SPREADSHEET_ID, range="21st Mar!F2",
-                              valueInputOption="USER_ENTERED", body={"values": price_on_appearance}).execute()
+        no_of_rows = no_of_stocks + 1
 
-        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                    range="Stocks_near_52_week_high!E3:E").execute()
-        change = result.get('values', [])
-        sheet.values().update(spreadsheetId=SPREADSHEET_ID, range="21st Mar!G2",
-                              valueInputOption="USER_ENTERED", body={"values": change}).execute()
+        # add_borders(sheet, rows=no_rows)
 
-        # request_body = {
-        #     "requests": [
-        #         {
-        #             # 'autoFill': {
-        #             #     "useAlternateSeries": False,
-        #             #     "sourceAndDestination": {
-        #             #         "source": {
-        #             #             "sheetId": 812605565,
-        #             #             "startRowIndex": 1,
-        #             #             "endRowIndex": 2,
-        #             #             "startColumnIndex": 2,
-        #             #             "endColumnIndex": 5
-        #             #         },
-        #             #         "dimension": "ROWS"
-        #             #     }
+        fill_column_data(
+            sheet, from_range="Stocks_near_52_week_high!F3:F", to_range="21st Mar!F2")
+        fill_column_data(
+            sheet, from_range="Stocks_near_52_week_high!E3:E", to_range="21st Mar!G2")
 
-        #             # }
-        #             "copyPaste": {
-        #                 "source": {
-        #                     "sheetId": 812605565,
-        #                     "startRowIndex": 1,
-        #                     "endRowIndex": 2,
-        #                     "startColumnIndex": 3,
-        #                     "endColumnIndex": 5
-        #                 },
-        #                 "destination": {
-        #                     "sheetId": 812605565,
-        #                     "startRowIndex": 1,
-        #                     "endRowIndex": 2,
-        #                     "startColumnIndex": 3,
-        #                     "endColumnIndex": 5
-        #                 },
-        #                 "pasteType": "PASTE_FORMULA"
-        #             }
-        #         }
-        #     ]
-        # }
-        # response = sheet.batchUpdate(spreadsheetId=SPREADSHEET_ID,
-        #                              body=request_body).execute()
-        # print(response)
+        copy_paste_cells(sheet, source={
+            "sheetId": 812605565,
+            "startRowIndex": 1,
+            "endRowIndex": 2,
+            "startColumnIndex": 3,
+            "endColumnIndex": 5
+        },
+            destination={
+            "sheetId": 812605565,
+            "startRowIndex": 2,
+            "endRowIndex": no_of_rows,
+            "startColumnIndex": 3,
+            "endColumnIndex": 5
+        })
+
+        copy_paste_cells(sheet, source={
+            "sheetId": 812605565,
+            "startRowIndex": 1,
+            "endRowIndex": 2,
+            "startColumnIndex": 7,
+            "endColumnIndex": 18
+        },
+            destination={
+            "sheetId": 812605565,
+            "startRowIndex": 2,
+            "endRowIndex": no_of_rows,
+            "startColumnIndex": 7,
+            "endColumnIndex": 18
+        })
+
+        fill_serial_nos(sheet, rows=no_of_rows)
 
     except HttpError as err:
         print(err)
